2b9c6b33ea99652934d5e6d9bd91c03d72731bca
Mustafa Evren <mustafaevrenmta@hotmail.com>
2023-04-12 01:05:39 +0300

    x
    x

--- blog.py
+++ blog.py
@@ -3,6 +3,19 @@ from flask_mysqldb import MySQL
 from wtforms import Form,StringField,TextAreaField,PasswordField,validators,EmailField
 from passlib.hash import sha256_crypt
 import email_validator
+from functools import wraps
+
+
+#User Login Decorator
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "logged_in" in session:
+            return f(*args, **kwargs)
+        else:
+            flash("For open this page, you must be logged in!","danger")
+            return redirect(url_for("login"))
+    return decorated_function
 
 #User Registration Form
 class RegisterForm(Form):
@@ -14,10 +27,17 @@ class RegisterForm(Form):
         validators.EqualTo(fieldname = "confirm",message = "Password does not match!")
     ])
     confirm = PasswordField("Confirm Password")
-    
+
+#User Login Form    
+class LoginForm(Form):
+    username = StringField("Username")
+    password = PasswordField("Password")    
 
 
 app = Flask(__name__)
+
+
+app.secret_key = "evorzblog"
 
 app.config["MYSQL_HOST"] = "localhost" #uzak sunucu olsaydı ip olacaktı
 app.config["MYSQL_USER"] = "root"
@@ -42,6 +62,14 @@ def about():
     return render_template("about.html")
 
 
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
+
+
+
+
 @app.route("/register",methods = ["GET", "POST"])
 def register():
     form = RegisterForm(request.form)
@@ -61,21 +89,83 @@ def register():
         mysql.connection.commit()
 
         cursor.close()
+        
+        flash("You have registered successfully.","success")
     
         return redirect(url_for("index"))
     
     else:
-        return render_template("register.html",form = form)
+        return render_template("login.html",form = form)
+    
+@app.route("/login", methods = ["GET","POST"])
+def login():
+    form = LoginForm(request.form)
+    if request.method == "POST":
+        username = form.username.data
+        password_entered = form.password.data
+    
+        cursor = mysql.connection.cursor()
+        
+        query = "Select * from users where username = %s"
+        
+        result = cursor.execute(query,(username,))
+        
+        if result > 0:
+            data = cursor.fetchone()
+            real_password = data["password"]
+            
+            if sha256_crypt.verify(password_entered,real_password):
+                flash("You have login successfully.","success")
+                
+                session["logged_in"] = True
+                session["username"] = username
+                
+                return redirect(url_for("index"))
+            else:
+                flash("You entered incorrect password!","danger")
+                return redirect(url_for("login"))
+            
+        else:
+            flash("There is no user with this username!","danger")
+            return redirect(url_for("login"))
+    
+    return render_template("login.html",form = form)
 
 
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
 
-@app.route("/article/<string:id>")
-def detail(id):
-    return "Article " + id
+#Add article
+@app.route('/addarticle',methods = ["GET", "POST"])
+def addarticle():
+    form = ArticleForm(request.form)
+    if request.method == "POST" and form.validate():
+        title = form.title.data
+        content = form.content.data
+        
+        cursor = mysql.connection.cursor()
+        
+        query = "Insert into articles(title,author,content) VALUES(%s,%s,%s)"
+        
+        cursor.execute(query,(title,session["username"],content))    
+        
+        mysql.connection.commit()    
+        
+        cursor.close()
+        
+        flash("Article successfully added.","success")
+        
+        return redirect(url_for("dashboard"))
+        
+    return render_template("addarticle.html",form = form)
 
-
-
-
+#Article Form
+class ArticleForm(Form):
+    title = StringField("Article Title",validators = [validators.length(min = 5, max = 100)])
+    content = TextAreaField("Article Content",validators = [validators.length(min = 10)])
+    
 
 
 
